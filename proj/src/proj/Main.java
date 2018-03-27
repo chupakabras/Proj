9e201f799ac5f71bafcbd67d81eb5056bcdc7f72
James Bond <James Bond@LAPTOP-T3S64R7G>
2018-03-27 16:46:20 +0300

    Created new class "ReadAndStore" to clean up Main class

--- proj/src/proj/Main.java
+++ proj/src/proj/Main.java
@@ -1,43 +1,12 @@
 package proj;
 
 public class Main {
-	private int rows;
 	
 	public static void main(String[] args) {
 		
-		Main kek = new Main();
+		ReadAndStore kek = new ReadAndStore();
 		GiftBean[] gift = kek.store();
 		kek.printAll(gift);
 	}
-	
-	public GiftBean[] store() {
-		
-		ReadFile r = new ReadFile();
-		r.openFile();
-		int rows = r.getNumberOfRows();
-		this.rows = rows;
-		String[] temp = new String[rows];
-		String[] wordsplit = new String[3];
-		
-		GiftBean[] gift = new GiftBean[rows];
-		
-		for(int i=0; i<rows; i++) {
-			gift[i] = new GiftBean();
-			temp[i] = r.getLine();
-			wordsplit = temp[i].split(" ");
-			wordsplit[0] = wordsplit[0].replace("_", " ");
-			gift[i].setName(wordsplit[0]);
-			gift[i].setDate(wordsplit[1]);
-			gift[i].setOwner(wordsplit[2]);
-		}
-		r.closeFile();
-		
-		return gift;
-	}
-	
-	public void printAll(GiftBean[] gift) {
-		for(int i=0; i<rows; i++) {
-			System.out.println(gift[i].toString());
-		}
-	}
 }
+
---
+++ proj/src/proj/ReadAndStore.java
@@ -0,0 +1,35 @@
+package proj;
+
+public class ReadAndStore{
+	private int rows;
+	public GiftBean[] store() {
+		
+		ReadFile r = new ReadFile();
+		r.openFile();
+		int rows = r.getNumberOfRows();
+		this.rows = rows;
+		String[] temp = new String[rows];
+		String[] wordsplit = new String[3];
+		
+		GiftBean[] gift = new GiftBean[rows];
+		
+		for(int i=0; i<rows; i++) {
+			gift[i] = new GiftBean();
+			temp[i] = r.getLine();
+			wordsplit = temp[i].split(" ");
+			wordsplit[0] = wordsplit[0].replace("_", " ");
+			gift[i].setName(wordsplit[0]);
+			gift[i].setDate(wordsplit[1]);
+			gift[i].setOwner(wordsplit[2]);
+		}
+		r.closeFile();
+		
+		return gift;
+	}
+	
+	public void printAll(GiftBean[] gift) {
+		for(int i=0; i<rows; i++) {
+			System.out.println(gift[i].toString());
+		}
+	}
+}
