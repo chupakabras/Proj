10cff9ff0c432cff914c73736386e6bf8c109b43
Ewa <Ewa@EDU-PC>
2018-03-28 22:13:05 +0300

    import

--- proj/src/proj/Interface.java
+++ proj/src/proj/Interface.java
@@ -3,5 +3,11 @@ package proj;
 public class Interface {
 	//java klasë, kurioje turëtø bûti pridëtas (apraðytas) interface 
 	//(nalabai ásivaizduoju kaip tai atrodys, tai neþinau ar tam reik atskiros klasës :D )
+	
+	import javax.swing.*;
+	import java.awt.*;
+	import java.awt.event.*;
+	
+	
 
 }
