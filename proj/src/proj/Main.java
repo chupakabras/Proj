252b0a3380afa67014c68a9de8ca4b0f56e455e9
Matas <Matas@DESKTOP-HLBQ0UD>
2018-03-30 18:53:28 +0300

    DatePicker

--- proj/src/proj/Main.java
+++ proj/src/proj/Main.java
@@ -11,18 +11,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		ReadAndStore kek = new ReadAndStore();
-		GiftBean[] gift = kek.store();
+	//	GiftBean[] gift = kek.store();
 		// kek.printAll(gift);
-		Table gui = new Table();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Table gui = new Table();
+/*		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gui.setSize(1000, 600);
 		gui.setVisible(true);
-		gui.setTitle("Gift list"); 
+		gui.setTitle("Gift list");  */
 
 		//System.out.println(gift[1].toString());
-		Interface usr = new Interface();
-		usr.setName();
-		System.out.println(usr.getName());
+	//	Interface usr = new Interface();
+	//	usr.setName();
+		//System.out.println(usr.getName());
+		
+		DatePickerTest j = new DatePickerTest();
+		j.start(null);
+		
 
 	}
 }
