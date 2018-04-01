f0c6084b70d3c6c3a09152bc55ed28b34f0b60b7
chupakabras <37798998+chupakabras@users.noreply.github.com>
2018-04-02 01:11:03 +0300

    Delete LogicAccessModifiers.java

--- proj/src/proj/LogicAccessModifiers.java
+++
@@ -1,7 +0,0 @@
-package proj;
-
-public class LogicAccessModifiers {
-
-	//java klasë, kuri nustatytø pagal esamà datà ir laikà ar dovana gali bûti nupirkta, kiek laiko liko iki jos galiojimo pabaigos
-	
-}
