17161791ae14822eaec0b5d3647dc4426bd2dccc
James Bond <James Bond@LAPTOP-T3S64R7G>
2018-04-02 00:37:45 +0300

    Labą dienukę

--- proj/src/proj/StartUP.java
+++ proj/src/proj/StartUP.java
@@ -39,7 +39,7 @@ public class StartUP {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 223, 87);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
