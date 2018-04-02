614ea0c73a615ed890ef5d9a253711c9f63bf060
James Bond <James Bond@LAPTOP-T3S64R7G>
2018-04-02 21:50:06 +0300

    adfs

---
+++ proj/src/proj/ReadAndStoreReserved.java
@@ -0,0 +1,35 @@
+package proj;
+
+public class ReadAndStoreReserved{
+	private int rows;
+	public ReservedBean[] store() {
+		
+		ReadFile r = new ReadFile();
+		r.openFile();
+		int rows = r.getNumberOfRows();
+		this.rows = rows;
+		String[] temp = new String[rows];
+		String[] wordsplit = new String[4];
+		
+		ReservedBean[] gift = new ReservedBean[rows];
+		
+		for(int i=0; i<rows; i++) {
+			gift[i] = new ReservedBean();
+			temp[i] = r.getLine();
+			wordsplit = temp[i].split(" ");
+			wordsplit[0] = wordsplit[0].replace("_", " ");
+			gift[i].setName(wordsplit[0]);
+			gift[i].setDate(wordsplit[1]);
+			
+		}
+		r.closeFile();
+		
+		return gift;
+	}
+	
+	public void printAll(GiftBean[] gift) {
+		for(int i=0; i<rows; i++) {
+			System.out.println(gift[i].toString());
+		}
+	}
+}
---
+++ proj/src/proj/ReadReserved.java
@@ -0,0 +1,38 @@
+package proj;
+import java.io.*;
+import java.util.*;
+
+public class ReadReserved {
+	
+	private Scanner f;
+	
+	public void openFile() {
+		try {
+			f = new Scanner(new File("ReservedData.txt"));
+		}
+		catch(Exception e) {
+			System.out.println("An error has occured while trying to read data file.");
+		}
+	}
+	
+	public void closeFile() {
+		f.close();
+	}
+	
+	public int getNumberOfRows() {
+		openFile();
+		int rows = 0;
+		while (f.hasNext()) {
+			f.nextLine();
+			rows++;
+		}
+		closeFile();
+		openFile();
+		return rows;
+	}
+	
+	public String getLine() {
+		String line = f.nextLine();
+		return line;
+	}
+}
